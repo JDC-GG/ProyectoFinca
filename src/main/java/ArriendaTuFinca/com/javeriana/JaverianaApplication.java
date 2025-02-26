d56441c41d769101f37d44f83f87e09534c57b73
JDC-GG <juan.cordoba0609@gmail.com>
2025-02-25 19:13:21 -0500

    Mensaje 1

---
+++ src/main/java/ArriendaTuFinca/com/javeriana/JaverianaApplication.java
@@ -0,0 +1,13 @@
+package ArriendaTuFinca.com.javeriana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaverianaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaverianaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ArriendaTuFinca/com/javeriana/JaverianaApplicationTests.java
@@ -0,0 +1,13 @@
+package ArriendaTuFinca.com.javeriana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaverianaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
